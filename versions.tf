002279fa13abfd7aa7c558037b40b3950718af2f
keith dutton <keith@vansanity.com>
2025-02-19 19:47:58 -0800

    rev versions

--- versions.tf
+++ versions.tf
@@ -1,10 +1,9 @@
 terraform {
-  required_version = ">= 1.3.0"
+  required_version = ">= 1.10.0"
 
   required_providers {
     aws = {
       source                = "hashicorp/aws"
-      version               = "~> 4.0"
       configuration_aliases = [aws.main, aws.cloudfront]
     }
   }
